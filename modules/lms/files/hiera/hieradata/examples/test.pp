eb40e8cfc4e7e49606cb589de52a495ef7db2080
Josh Samuelson <js@puppetlabs.com>
2015-06-12 14:30:04 -0700

    More explicit test manifest

--- modules/lms/files/hiera/hieradata/examples/test.pp
+++ modules/lms/files/hiera/hieradata/examples/test.pp
@@ -1,1 +1,11 @@
-notice(hiera_hash("site_users"))
+$site_users = hiera_hash("site_users")
+
+notice('jane')
+notice($site_users['jane'])
+
+notice('jim')
+notice($site_users['jim'])
+
+notice('bob')
+notice($site_users['bob'])
+
