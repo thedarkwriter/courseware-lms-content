bbf01f25b542ee55cc3741d1026675ca6c813acf
Josh Samuelson <js@puppetlabs.com>
2015-06-23 15:02:56 -0700

    Example code

---
+++ modules/lms/files/infrastructure/environments/production/modules/profiles/manifests/mysqldb.pp
@@ -0,0 +1,22 @@
+class profile::mysqldb {
+  class { 'mysql::server':
+    root_password => 'supersekrit',
+  }
+  mysql::db {'wordpress':
+    user     => 'wordpress',
+    password => 'wordpress',
+    host     => 'localhost',
+    grant    => ['ALL']
+  }
+  mysql_grant { 'wordpress@%/*.*':
+    ensure     => 'present',
+    options    => ['GRANT'],
+    privileges => ['ALL'],
+    table      => '*.*',
+    user       => 'wordpress@%',
+  }
+  mysql_user { 'wordpress@%':
+    ensure   => 'present',
+    password_hash => mysql_password('wordpress')
+  }
+}
