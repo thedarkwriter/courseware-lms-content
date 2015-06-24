7da1e2e3bd8ce976690b2abf7e466cc99ba00fc9
Josh Samuelson <js@puppetlabs.com>
2015-06-24 13:14:55 -0700

    Clean up update script

--- scripts/update.pp
+++ scripts/update.pp
@@ -3,24 +3,5 @@ exec { 'update repo':
   cwd     => '/usr/src/courseware-lms-content',
   path    => '/usr/bin:/bin'
 }
-file { '/etc/puppetlabs/puppet/modules/':
-  ensure  => directory,
-  source  => '/usr/src/courseware-lms-content/modules/',
-  recurse => true,
-  require => Exec['update repo']
-}
-file { '/etc/puppetlabs/puppet/hieradata/':
-  source  => '/usr/src/courseware-lms-content/hiera/hieradata/',
-  recurse => true,
-  require => Exec['update repo']
-}
-file { '/etc/puppetlabs/puppet/hiera.yaml':
-  source  => '/usr/src/courseware-lms-content/hiera/hiera.yaml',
-  require => Exec['update repo']
-}
-file { '/usr/local/bin/course_selector':
-  ensure  => present,
-  mode    => 755,
-  source  => '/usr/src/courseware-lms-content/scripts/course_selector.rb',
-  require => Exec['update repo']
-}
+include lms::lab_repo
+include lms::course_selector
