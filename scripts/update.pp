ba334c1e6de32f32f7570777ee2ccf0b8022de9a
Josh Samuelson <js@puppetlabs.com>
2015-06-24 13:18:57 -0700

    Only update lms module

--- scripts/update.pp
+++ scripts/update.pp
@@ -3,5 +3,15 @@ exec { 'update repo':
   cwd     => '/usr/src/courseware-lms-content',
   path    => '/usr/bin:/bin'
 }
-include lms::lab_repo
-include lms::course_selector
+file { '/etc/puppetlabs/puppet/modules/lms/':
+  ensure  => directory,
+  source  => '/usr/src/courseware-lms-content/modules/lms/',
+  recurse => true,
+  require => Exec['update repo']
+}
+file { '/usr/local/bin/course_selector':
+  ensure  => present,
+  mode    => 755,
+  source  => '/usr/src/courseware-lms-content/scripts/course_selector.rb',
+  require => Exec['update repo']
+}
