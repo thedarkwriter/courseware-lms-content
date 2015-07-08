c6fd3a8e1e7b86419a5d5c79930de399148f8e2c
Josh Samuelson <js@puppetlabs.com>
2015-07-08 16:25:15 -0700

    Better menu

--- scripts/course_menu.rb
+++ scripts/course_menu.rb
@@ -1,60 +1,49 @@
 #! /usr/bin/ruby
 
 self_paced = {
-               "1"  => "resources.pp",
-               "2"  => "relationships.pp",
-               "3"  => "hiera_intro.pp",
-               "4"  => "puppet_lint.pp",
-               "5"  => "inheritance.pp",
-               "6"  => "autoloading.pp",
-               "7"  => "facter_intro.pp",
-               "8"  => "vim_intro.pp",
-               "9"  => "cli_intro.pp",
-               "10" => "classes.pp",
-               "11" => "testing.pp",
-               "12" => "validating.pp",
-             }
+  "A"  => { :name => "Resources", :file => "resources.pp"},
+  "B"  => { :name => "Relationships", :file => "relationships.pp"},
+  "C"  => { :name => "An Introduction to Hiera", :file => "hiera_intro.pp"},
+  "D"  => { :name => "Puppet Lint", :file => "puppet_lint.pp"},
+  "E"  => { :name => "Inheritance", :file => "inheritance.pp"},
+  "F"  => { :name => "Autoloading", :file => "autoloading.pp"},
+  "G"  => { :name => "An Introduction to Facter", :file => "facter_intro.pp"},
+  "H"  => { :name => "An Introduction to Vim", :file => "vim_intro.pp"},
+  "I"  => { :name => "An Introduction to the Linux Command Line", :file => "cli_intro.pp"},
+  "J" => { :name => "Classes", :file => "classes.pp"},
+  "K" => { :name => "Testing", :file => "testing.pp"},
+  "L" => { :name => "Validating Puppet Code", :file => "validating.pp"},
+}
 
 instructor_led = {
-                    "1" => "hiera.pp",
-                    "2" => "module.pp",
-                    "3" => "code.pp",
-                    "4" => "infrastructure.pp",
-                    "5" => "default.pp"
-                 }
+  "A" => { :name => "Practical Hiera Usage", :file => "hiera.pp"},
+  "B" => { :name => "Writing Your First Module", :file => "module.pp"},
+  "C" => { :name => "Managing Puppet Code", :file => "code.pp"},
+  "D" => { :name => "Infrastructure Design Using Puppet Modules", :file => "infrastructure.pp"},
+  "E" => { :name => "Other Courses", :file => "default.pp"},
+}
 
 puts "-----------------------------------------"
-puts " [1] Resources"
-puts " [2] Relationships"
-puts " [3] An Introduction to Hiera"
-puts " [4] Puppet Lint"
-puts " [5] Inheritance"
-puts " [6] Autoloading"
-puts " [7] An Introduction to Facter"
-puts " [8] An Introduction to Vim"
-puts " [9] An Introduction to the Linux Command Line"
-puts "[10] Classes"
-puts "[11] Testing"
-puts "[12] Validating Puppet Code"
-puts " [I] Instructor Led Courses"
+self_paced.sort_by{|k,v|k}.each do |letter, course|
+  printf("[%s] %s\n", letter, course[:name])
+end
+puts "[Z] Instructor Led Courses"
 puts "-----------------------------------------"
-puts "Enter course number:"
-course_number = gets().chomp
+puts "Enter course letter:"
+course_letter = gets().chomp.upcase
 
-if self_paced.has_key?(course_number)
-then
-  %x(puppet apply /etc/puppetlabs/puppet/modules/lms/tests/#{self_paced[course_number]})
-else
-  puts "[1] Practical Hiera Usage"
-  puts "[2] Writing Your First Module"
-  puts "[3] Managing Puppet Code"
-  puts "[4] Infrastructure Design Using Puppet Modules"
-  puts "[5] Other Courses"
-  puts "Enter course number:"
-  course_number = gets().chomp
-  if instructor_led.has_key?(course_number)
+if self_paced.has_key?(course_letter)
   then
-    %x(puppet apply /etc/puppetlabs/puppet/modules/lms/tests/#{instructor_led[course_number]})
+  %x(puppet apply /etc/puppetlabs/puppet/modules/lms/tests/#{self_paced[course_letter][:file]})
+else
+  instructor_led.sort_by{|k,v|k}.each do |letter, course|
+    printf("[%s] %s\n", letter, course[:name])
+  end
+  puts "Enter course letter:"
+  course_number = gets().chomp.upcase
+  if instructor_led.has_key?(course_letter)
+    then
+    %x(puppet apply /etc/puppetlabs/puppet/modules/lms/tests/#{instructor_led[course_letter][:file]})
   end
 end
 
