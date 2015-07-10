348eb6b9f7b19d7d0807a337e614d55206a7b14c
Josh Samuelson <js@puppetlabs.com>
2015-07-10 12:10:13 -0700

    Manage bashrc and delete tmux conf

--- modules/lms/tests/troubleshooting.pp
+++ modules/lms/tests/troubleshooting.pp
@@ -11,6 +11,10 @@ file {'/usr/local/bin/':
   mode    => 755,
 }
 file {'/root/.bashrc.puppet':
+  ensure => present,
+  source => "puppet:///modules/lms/troubleshooting/bashrc.puppet",
+}
+file {'/root/.tmux.conf':
   ensure => absent,
 }
 
