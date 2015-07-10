b9967bc99fda0af80c5e8e05d0c436aa4a897d81
Josh Samuelson <js@puppetlabs.com>
2015-07-10 11:14:11 -0700

    Files and code for putting them in place

--- modules/lms/tests/troubleshooting.pp
+++ modules/lms/tests/troubleshooting.pp
@@ -1,2 +1,13 @@
 class{'lms': course => 'default'}
+file {'/root/labs/':
+  ensure  => directory,
+  recurse => true,
+  source  => "puppet:///modules/lms/troubleshooting/labs",
+}
+file {'/usr/local/bin/':
+  ensure  => directory,
+  recurse => true,
+  source  => "puppet:///modules/lms/troubleshooting/scripts/",
+  mode    => 755,
+}
 
