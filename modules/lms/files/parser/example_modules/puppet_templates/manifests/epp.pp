02e74a1e616ffa5f2e007e17236fa129847ebdd9
Josh Samuelson <js@puppetlabs.com>
2015-07-24 14:12:48 -0700

    Starter code for motd parser lab

--- modules/lms/files/parser/example_modules/puppet_templates/manifests/epp.pp
+++ modules/lms/files/parser/example_modules/puppet_templates/manifests/epp.pp
@@ -1,7 +1,5 @@
 class puppet_templates::epp {
   file { '/etc/motd':
-    content => epp("puppet_templates/motd.epp", { 
-      'student_name' => "Enter your name",
-      }),
+    content =>
   }
 }
