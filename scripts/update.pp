ba1a4b04ebb36c8afa1d3227d14365d450ef1f75
Josh Samuelson <js@puppetlabs.com>
2015-07-29 13:29:56 -0700

    Select codedir based on version

--- scripts/update.pp
+++ scripts/update.pp
@@ -1,23 +1,28 @@
+$codedir = versioncmp('4.0.0',$puppetversion) ? {
+  1       => '/etc/puppetlabs/puppet',
+  default => '/etc/puppetlabs/code'
+}
+
 exec { 'update repo':
   command => 'git pull',
   cwd     => '/usr/src/courseware-lms-content',
   path    => '/usr/bin:/bin'
 }
-file { '/etc/puppetlabs/puppet/modules/lms/':
+file { "${codedir}/modules/lms/":
   ensure  => directory,
   source  => '/usr/src/courseware-lms-content/modules/lms/',
   recurse => true,
   require => Exec['update repo']
 }
 file { '/usr/local/bin/course_selector':
-  ensure  => present,
-  mode    => 755,
+  ensure  => file,
+  mode    => '0755',
   source  => '/usr/src/courseware-lms-content/scripts/course_selector.rb',
   require => Exec['update repo']
 }
 file { '/usr/local/bin/course_menu':
-  ensure  => present,
-  mode    => 755,
+  ensure  => file,
+  mode    => '0755',
   source  => '/usr/src/courseware-lms-content/scripts/course_menu.rb',
   require => Exec['update repo']
 }
