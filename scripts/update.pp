562804be574dd60f0f93d959ed28f03b7791fccd
Josh Samuelson <js@puppetlabs.com>
2015-07-30 12:33:09 -0700

    Comment about aio_agent

--- scripts/update.pp
+++ scripts/update.pp
@@ -1,3 +1,6 @@
+# Normally this would use the aio_agent_version fact
+# that is installed by the puppet_enterprise module
+# since we're installing puppet-agent, we'll use this fact instead
 $codedir = versioncmp('4.0.0',$puppetversion) ? {
   1       => '/etc/puppetlabs/puppet',
   default => '/etc/puppetlabs/code'
