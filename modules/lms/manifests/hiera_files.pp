5986aa9665ec0c220647db9bf6b19cce38fd98a5
Josh Samuelson <js@puppetlabs.com>
2015-07-30 14:53:31 -0700

    Fixing codedir path

--- modules/lms/manifests/hiera_files.pp
+++ modules/lms/manifests/hiera_files.pp
@@ -1,9 +1,9 @@
 class lms::hiera_files {
-  file { "${codedir}/hiera.yaml":
+  file { "${lms::codedir}/hiera.yaml":
     ensure => present,
     source => "puppet:///modules/lms/${lms::course}/hiera.yaml",
   }
-  file { "${codedir}/hieradata/":
+  file { "${lms::codedir}/hieradata/":
     ensure  => directory,
     recurse => true,
     purge   => true,
