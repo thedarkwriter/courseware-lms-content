5cb906a9c8ded5dcfed9d8d0c26b112cec05a3b4
Josh Samuelson <js@puppetlabs.com>
2015-08-04 14:52:39 -0700

    making the bool parameter a string

--- modules/lms/files/parser/example_modules/data_types/manifests/params.pp
+++ modules/lms/files/parser/example_modules/data_types/manifests/params.pp
@@ -1,7 +1,7 @@
 class data_types::params {
     $example_string = "This is the message of the day"
     $example_integer = 2
-    $example_bool = true
+    $example_bool = 'false'
     $example_array = ['zip','zap','zop']
     $example_hash = {'fortune-mod' => { 
         ensure => present,
