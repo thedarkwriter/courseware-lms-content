7c10466d4803b806fc9003b28d5da2d13172d031
Josh Samuelson <js@puppetlabs.com>
2015-08-04 20:41:02 -0700

    Fixed mode and quotes

--- modules/lms/manifests/course/troubleshooting.pp
+++ modules/lms/manifests/course/troubleshooting.pp
@@ -3,12 +3,12 @@ class lms::course::troubleshooting {
   file {'/usr/local/bin/':
     ensure  => directory,
     recurse => true,
-    source  => "puppet:///modules/lms/troubleshooting/scripts/",
-    mode    => 755,
+    source  => 'puppet:///modules/lms/troubleshooting/scripts/',
+    mode    => '0755',
   }
   file {'/root/.bashrc.puppet':
     ensure => present,
-    source => "puppet:///modules/lms/troubleshooting/bashrc.puppet",
+    source => 'puppet:///modules/lms/troubleshooting/bashrc.puppet',
   }
   file {'/root/.tmux.conf':
     ensure => absent,
