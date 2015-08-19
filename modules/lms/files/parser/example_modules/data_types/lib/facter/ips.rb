714f53fce3ed5c3dab36c22ebf4befa0f92affac
Josh Samuelson <js@puppetlabs.com>
2015-08-19 16:28:01 -0700

    Removing unused custom fact

--- modules/lms/files/parser/example_modules/data_types/lib/facter/ips.rb
+++
@@ -1,9 +0,0 @@
-Facter.add("ips") do
-  setcode do
-    #Get all the network interfaces except lo
-    interfaces = Facter.value("networking")["interfaces"]
-    interfaces.delete("lo")
-    ips = interfaces.collect {|k, v| v["ip"]}
-    ips
-  end
-end
