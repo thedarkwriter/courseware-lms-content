b279775815937f305fdfd84d3319bc148e97440e
Josh Samuelson <js@puppetlabs.com>
2015-08-19 14:52:33 -0700

    Create the /root/puppetcode dir

--- modules/lms/manifests/course/parser.pp
+++ modules/lms/manifests/course/parser.pp
@@ -1,11 +1,14 @@
 class lms::course::parser {
   include lms
-  file { "/root/puppetcode/modules":
+  file { '/root/puppetcode':
+    ensure => directory,
+  }
+  file { '/root/puppetcode/modules':
     ensure  => directory,
     recurse => true,
     source  => 'puppet:///modules/lms/parser/example_modules',
   }
-  exec {"puppet module install jfryman-nginx --modulepath=/root/puppetcode/modules":
+  exec {'puppet module install jfryman-nginx --modulepath=/root/puppetcode/modules':
     path => '/usr/local/bin:/bin'
   }
 }
