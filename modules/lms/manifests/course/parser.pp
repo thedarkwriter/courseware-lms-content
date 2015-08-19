e881d2049408accd70bb3cf6d9989badcf13f011
Josh Samuelson <js@puppetlabs.com>
2015-08-19 13:38:57 -0700

    Put starter code in /root/puppetcode

--- modules/lms/manifests/course/parser.pp
+++ modules/lms/manifests/course/parser.pp
@@ -1,11 +1,11 @@
 class lms::course::parser {
   include lms
-  file { "$settings::codedir/modules":
+  file { "/root/puppetcode/modules":
     ensure  => directory,
     recurse => true,
     source  => 'puppet:///modules/lms/parser/example_modules',
   }
-  exec {"puppet module install jfryman-nginx --modulepath=$settings::codedir/modules":
+  exec {"puppet module install jfryman-nginx --modulepath=/root/puppetcode/modules":
     path => '/usr/local/bin:/bin'
   }
 }
