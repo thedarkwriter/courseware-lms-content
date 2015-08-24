2d1766c31ca8c355a856180b112ac65a4b2913a8
Josh Samuelson <js@puppetlabs.com>
2015-08-24 14:47:01 -0700

    Run course update script

--- scripts/course_selector.rb
+++ scripts/course_selector.rb
@@ -14,5 +14,8 @@ puts "Updating course content, please stand by."
 # Update courseware content
 %x(puppet apply /usr/src/courseware-lms-content/scripts/update.pp)
 
+# Run course_update script
+%x(/usr/local/bin/course_update)
+
 # Run Course Menu
 exec '/usr/local/bin/course_menu'
