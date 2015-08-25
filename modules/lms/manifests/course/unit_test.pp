26191c64d543b6d722b73f5896317d283dabe16b
Josh Samuelson <js@puppetlabs.com>
2015-08-25 10:18:59 -0700

    Added files and menu item for unit tests

---
+++ modules/lms/manifests/course/unit_test.pp
@@ -0,0 +1,12 @@
+class lms::course::unit_test {
+  include lms
+  file { "${settings::codedir}/modules":
+    ensure  => directory,
+    recurse => true,
+    source  => 'puppet:///modules/lms/unit_test/problem_modules',
+  }
+  # Students install these packages during the course, so remove them here
+  package { ['rspec-puppet','puppetlabs_spec_helper']
+    ensure => absent,
+  }
+}
