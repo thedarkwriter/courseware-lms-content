d06305f490c7c5b0869e30c5a77aca2b3db74d93
Josh Samuelson <js@puppet.com>
2017-03-14 10:02:41 -0700

    Stop slides timers from stomping eachother

--- assets/timeline.js
+++ assets/timeline.js
@@ -1,23 +1,37 @@
-function timeline(times, slidename) {
-  slide = $(".slide." + slidename);
+var timers = []
+
+function clearAllTimers(t){
+  t.forEach(function(timerid){
+    clearTimeout(timerid);
+  });
+  timers = []
+}
+
+function bind_times(times,slide){
   slide.bind("showoff:show", function (event) {
+    clearAllTimers(timers);
     times.forEach(function(time,index) {
       $(".time" + (index + 1)).css('visibility', 'hidden');
-      setTimeout(function(){
+      timers.push(setTimeout(function(){
         $(".time" + (index + 1)).css('visibility', 'visible');
-      },time);
+      },time));
     });
+  });
+  slide.bind("showoff:prev", function (event) {
+    clearAllTimers(timers);
+  });
+  slide.bind("showoff:next", function (event) {
+    clearAllTimers(timers);
   });
 }
 
-function audio(slidename) {
+function timeline(times, slidename) {
   slide = $(".slide." + slidename);
-  slide.append(`
-    <audio id="${slidename}">
-      <source src="file/audio/${slidename}.mp3" type="audio/mpeg">
-    </audio>`);
-  player = $("#" + slidename)[0];
-  slide.bind("showoff:show", function (event) {
+  bind_times(times,slide);
+}
+
+function bind_audio(slide, player){
+  slide.bind("showoff:show", function(event){
     player.currentTime = 0;
     player.play();
   });
@@ -27,4 +41,14 @@ function audio(slidename) {
   slide.bind("showoff:next", function (event) {
     player.pause();
   });
+}
+
+function audio(slidename){
+  slide = $(".slide." + slidename);
+  slide.append(`
+    <audio id="${slidename}">
+      <source src="file/audio/${slidename}.mp3" type="audio/mpeg">
+    </audio>`);
+  player = $("#" + slidename)[0];
+  bind_audio(slide,player);
 }
